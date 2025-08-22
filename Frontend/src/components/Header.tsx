d11c90ddb4f30832e231bf259f7c66db91ff13e0
Gryllex <Gryllex@gmail.com>
2025-08-22 15:05:52 +0200

    feat: add main, sponsors, and training-benefits sections to landing page

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,18 +1,34 @@
 import { Link } from "react-router-dom";
 import '../css-styles/header.css'
 import { useAuth } from "../hooks/useAuth";
+import { useState } from "react";
 
 export function Header () {
     const { isAuthenticated } = useAuth()
+    const [mobileCheckbox, setMobileCheckbox] = useState<boolean>(false)
 
     return (
             <header className="main-page-header">
-                <div className="header-wrapper">
+                {/* MOBILE CHECKBOX */}
+                <div className="header-mobile-checkbox-container">
+                    <label htmlFor="header-mobile-checkbox">
+                        <img src={ mobileCheckbox ? '../src/assets/menu-bar-x.png' : '../src/assets/menu-bar.png'} alt="" />
+                    </label>
+                    <input type="checkbox" id="header-mobile-checkbox"
+                        checked={mobileCheckbox}
+                        onChange={()=>{setMobileCheckbox(!mobileCheckbox)}}
+                    />
+                </div>
+
+                {/* LOGO */}
+                <div className="header-logo">
+                    <Link to='/'>
+                        <img src="https://static.vecteezy.com/system/resources/previews/026/109/417/original/gym-logo-fitness-health-muscle-workout-silhouette-design-fitness-club-free-vector.jpg" alt="Logo" />
+                    </Link>
+                </div>
+                <div  className={mobileCheckbox ? 'header-wrapper display-mobile-header' : 'header-wrapper'}>
                     {/* MAIN NAV */}
                     <nav className="left-header">
-                        <Link to='/'>
-                            <img src="https://static.vecteezy.com/system/resources/previews/026/109/417/original/gym-logo-fitness-health-muscle-workout-silhouette-design-fitness-club-free-vector.jpg" alt="Logo" />
-                        </Link>
                         <Link to='/training' >Training</Link>
                         <Link to='/merchandising' >Merchandising</Link>
                     </nav>
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { Header } from "../components/Header"
 import '../css-styles/home.css'
 
@@ -5,7 +6,66 @@ export default function Home () {
     return(
         <>
             < Header />
-            <div className="provisional-body"></div>
+            <div className="page-container">
+                {/* <main className="home-main-section home-section">
+                </main> */}
+
+                    <section className="main-section home-section">
+                        <div className="main-section-info">
+                            <h1 className="hero-title">
+                                Find your <span>personal trainer</span> and book your next session within minutes!
+                            </h1>
+                            <p>
+                                We connect clients and trainers quickly and easily, Sign up today, book workouts as a client or grow your business as a trainer
+                            </p>
+                            <Link to='/register' >
+                                <button>Join now</button>
+                            </Link>
+                        </div>
+                    </section>
+
+
+                {/* SPONSORS */}
+                <section className="sponsor-section home-section">
+                    <div className="sponsor-container">
+                        <img src="../src/assets/sponsors1.png" alt="" />
+                        <img src="../src/assets/sponsors2.png" alt="" />
+                    </div>
+                </section>
+
+
+                <section className="training-benefits home-section">
+                    <div className="training-benefits__card-container">
+                        <div className="training-benefits__card">
+                            <img src="../src/assets/muscle-hypertrophy.png" alt=""/>
+                            <p>Muscle hypertrophy</p>
+                        </div>
+                        <div className="training-benefits__card">
+                            <img src="../src/assets/build-strength.png" alt=""/>
+
+                            <p>Build strength</p>
+                        </div>
+                        <div className="training-benefits__card">
+                            <img src="../src/assets/improve-posture.png" alt=""/>
+
+                            <p>Improved Balance & Posture</p>
+                        </div>
+                        <div className="training-benefits__card">
+                            <img src="../src/assets/motivation.png" alt=""/>
+                            <p>Motivation</p>
+                        </div>
+                        <div className="training-benefits__card">
+                            <img src="../src/assets/mental-health.png" alt=""/>
+                            <p>Mental health</p>
+                        </div>
+                        <div className="training-benefits__card">
+                            <img src="../src/assets/cardiovascular-health.png" alt=""/>
+                            <p>Cardiovascular Health</p>
+                        </div>
+                    </div>
+                </section>
+                <section className="home-section"></section>
+            </div>
         </>
     )
 }
